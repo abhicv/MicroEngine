4caa48a8e81865062d192c9ea0ea6c2cabe0c2b4
Lone-coder <abhijithcheruvery@gmail.com>
2020-03-12 08:17:04 +0530

    Added Vector_Field.c

---
+++ src/Vector_Field.c
@@ -0,0 +1,12 @@
+#include "../../include/MicroEngine/MicroEngine.h"
+
+void main(int argc, char *argv[])
+{
+    if(!ME_Init("MicroUI test",800,600))
+    {
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    return
+}
